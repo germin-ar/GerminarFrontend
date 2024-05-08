b1652297456e733472829088b9a8b1819914b477
Mariano <mariano_ariel_97@outlook.com>
2024-05-08 20:15:58 -0300

    Proyecto Base

---
+++ src/app/biblioteca/page.tsx
@@ -0,0 +1,5 @@
+export default function BibliotecaPage(){
+    return(
+        <div>Biblioteca</div>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <main className="flex justify-center items-center">
+        <img src="/trastornos-y-enfermedades-tomate-scaled.webp" alt="tomate" width="1000"/>
+      </main>
+  );
+}
