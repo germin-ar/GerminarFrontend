56624243ffb92b2536b45022a2d17e176966c3c1
Agostina <aherdeiro@alumno.unlam.edu.ar>
2024-05-17 21:47:19 -0300

    mejoras en el responsive

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,23 +8,24 @@ import Image from "next/image";
 export default function Footer() {
     return (
         <footer
-            className={`${stylesFooter.footer} flex flex-col lg:flex-row items-center justify-center p-6 bg-gray-800`}>
+            className={`${stylesFooter.footer} flex flex-col lg:flex-row items-center justify-center gap-8 p-6`}>
             <div
-                className={`${stylesFooter.borde} flex-1 relative w-full lg:w-1/3 flex flex-col items-center justify-center gap-5 mb-6 lg:mb-0 lg:border-0`}>
-                <div className="w-24 h-24 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-16 lg:h-16">
+                className={`${stylesFooter.borde} flex-1 relative w-full lg:w-1/3 flex flex-col items-center justify-center gap-2 lg:border-0`}>
+                <div className="w-14 h-14 sm:w-16 sm:h-16  md:w-16 md:h-16 lg:w-16 lg:h-16">
                     <Image src="/isotipo-fondo-claro.png" alt="usuario prueba" width={100} height={100}
                            layout="responsive"/>
                 </div>
-                <div className="w-32 h-8 sm:w-40 sm:h-10 md:w-48 md:h-12 lg:w-56 lg:h-12">
+                <div className="w-32 sm:w-34 md:w-26 lg:w-36">
                     <Image src="/logotipo-claro.png" alt="usuario prueba" width={250} height={50} layout="responsive"/>
                 </div>
             </div>
-            <div className="flex-1 w-full lg:w-2/3 flex flex-col lg:flex-row justify-around items-center gap-10">
-                <ul className={`${stylesFooter.menu} flex flex-col lg:flex-row items-center justify-around text-center text-wrap w-full`}>
-                    <li className="text-white text-2xl w-full lg:w-32"><a href="#">Home</a></li>
-                    <li className="text-white text-2xl w-full lg:w-32"><a href="#">Servicios</a></li>
-                    <li className="text-white text-2xl w-full lg:w-32"><a href="#">Sobre nosotros</a></li>
-                    <li className="text-white text-2xl w-full lg:w-32"><a href="#">Centro de ayuda</a></li>
+            <div className="flex-1 w-full lg:w-2/3 flex flex-col lg:flex-row justify-around items-center">
+                <ul className={`${stylesFooter.menu} flex flex-col lg:flex-row items-center justify-around text-center text-wrap w-full gap-1`}>
+                    <li className="text-white text-lg w-full lg:w-32"><a href="#">Home</a></li>
+                    <li className="text-white text-lg w-full lg:w-32"><a href="#">Servicios</a></li>
+                    <li className="text-white text-lg w-full lg:w-32"><a href="#">Sobre nosotros</a></li>
+                    <li className="text-white text-lg w-full lg:w-32"><a href="#">Centro de ayuda</a></li>
+                    <li className="text-white text-lg w-full lg:w-32"><a href="#">Contacto</a></li>
                 </ul>
             </div>
             <div className="flex-1">
