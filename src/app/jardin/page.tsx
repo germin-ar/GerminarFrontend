1ce13ac536a0239690d3c0284dc2cc04058aff36
1Mariano <mariano_ariel_97@outlook.com>
2024-06-05 16:26:39 -0300

    wip

--- src/app/jardin/page.tsx
+++ src/app/jardin/page.tsx
@@ -2,21 +2,32 @@
 import stylesJardin from "@/app/jardin/jardin.module.css"
 import {BalooBhaina2} from "@/app/ui/fonts";
 import {CiCalendar} from "react-icons/ci";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {IoIosHome} from "react-icons/io";
 import {PiPottedPlantFill} from "react-icons/pi";
 import {FaClock, FaHeart} from "react-icons/fa";
 import Image from "next/image";
 import Link from "next/link";
 
-interface Planta {
-    nombre: string;
-    foto: string;
-    especifico: { nombre: string; foto: string; }[];
+interface Plant {
+    id: number;
+    alias: string;
+    creation_date: string;
+    modification_date: string;
 }
 
+interface Garden {
+    id: number;
+    name: string;
+    user: {
+        id: number;
+        name: string;
+        email: string;
+    };
+    plants: Plant[]; // Aquí se define la propiedad plants como un array de objetos de tipo Plant
+}
 export default function JardinPage() {
-    const data = [
+    /*const data = [
         {
             categoria: 'Balcón',
             plantas: [
@@ -91,7 +102,7 @@ export default function JardinPage() {
                 }
             ]
         }
-    ];
+    ];*/
 
     const [filtro, setFiltro] = useState('');
 
@@ -99,11 +110,11 @@ export default function JardinPage() {
         setFiltro(e.target.value);
     };
 
-    const filteredData = data.filter((categoria) => {
+    /*const filteredData = data.filter((categoria) => {
         const categoriaIncluida = categoria.categoria.toLowerCase().includes(filtro.toLowerCase());
         const plantasFiltradas = categoria.plantas.filter((planta) => planta.nombre.toLowerCase().includes(filtro.toLowerCase()));
         return categoriaIncluida || plantasFiltradas.length > 0;
-    });
+    });*/
 
     /**sidebar**/
     const [ubicacionVisible, setUbicacionVisible] = useState(false);
@@ -122,15 +133,37 @@ export default function JardinPage() {
 
     /****/
     const [popupVisible, setPopupVisible] = useState(false);
-    const [plantaSeleccionada, setPlantaSeleccionada] = useState<Planta | null>(null); //
-    const [cat, setCat] = useState("");
+    const [plantaSeleccionada, setPlantaSeleccionada] = useState<Plant | null>(null); //
+    const [cat, setCat] = useState(0);
 
-    const mostrarPopup = (planta: Planta, categoria: string) => {
+    const mostrarPopup = (planta: Plant, idPlanta: number) => {
         setPopupVisible(true);
         setPlantaSeleccionada(planta);
-        setCat(categoria);
+        setCat(idPlanta);
         console.log(planta)
     };
+
+    const [gardens, setGardens] = useState<Garden[]>([]);
+
+    const fetchUbicaciones = async () => {
+        try {
+            const response = await fetch('http://localhost:8080/api/v1/gardens', {
+                headers: {
+                    'id-user': '1'
+                }
+            });
+            const data = await response.json();
+            console.log(data);
+            setGardens(data);
+
+        } catch (error) {
+            console.error('Error al obtener las ubicaciones:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchUbicaciones();
+    }, []);
 
     return (
         <>
@@ -169,8 +202,8 @@ export default function JardinPage() {
                         </div>
                     </div>
                 </section>
-                <div className={`${stylesJardin.contenidoAjustado} flex mb-10`}>
-                    <div className="w-72 flex-shrink-0">
+                <div className={`${stylesJardin.contenidoAjustado} flex mb-10  `}>
+                    <div className="w-72 flex-shrink-0 bg-[#88BC43]">
                         <ul className="pl-5 pt-5 select-none">
                             <div>
                                 <div className="flex items-center gap-1 ">
@@ -289,51 +322,66 @@ export default function JardinPage() {
                         </ul>
                     </div>
                     <section className="flex-1 flex gap-10 flex-col pl-10 pt-10 pb-10">
-                        {filteredData.map((categoria, index) => (
-                            <div className="flex flex-col flex-wrap" key={index}>
-                                <h2 className={`${BalooBhaina2.className} text-[#88BC43] font-bold`}>{categoria.categoria}</h2>
-                                <div className={`flex gap-10 flex-wrap`}>
-                                    {categoria.plantas.map((planta, idx) => (
-                                        <div key={idx}> {/* Aquí he agregado un div contenedor */}
-                                            <h3 className={`${BalooBhaina2.className} text-center`}>{planta.nombre} {planta.especifico ? planta.especifico.length : 0} </h3>
-                                            <div>
-                                                <Image
-                                                    className={`sm:w-96 sm:h-60 rounded shadow-lg border-2 border-green-800 cursor-pointer`}
-                                                    onClick={() => mostrarPopup(planta, categoria.categoria)}
-                                                    src={`${planta.foto}`} alt={`${planta.nombre}`}
-                                                    width="250" height="250"/>
-                                            </div>
-                                        </div>
-                                    ))}
+                        <div>
+                            {/* Renderizar ubicaciones */}
+                            {gardens.map(garden => (
+                                <div key={garden.id}>
+                                    <h2>{garden.name}</h2>
+                                    <ul>
+                                        {garden.plants.map(planta => (
+                                            <li onClick={() => mostrarPopup(planta, planta.id)} key={planta.id}>{planta.alias}</li>
+                                        ))}
+                                    </ul>
                                 </div>
-                            </div>
-                        ))}
-                        {popupVisible && plantaSeleccionada && (
-                            <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
-                                <div className="bg-white p-8 rounded-lg ">
-                                    <div>
-                                        <h2 className={`${BalooBhaina2.className} text-[#88BC43] font-bold`}>{cat}</h2>
-                                        <ul className="flex items-center justify-center gap-5">
-                                            {plantaSeleccionada.especifico.map((detalle, index) => (
+                            ))}
+                        </div>
+                        {popupVisible && plantaSeleccionada &&(
+                            <>
+                                <div
+                                    className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
+                                    <div className="bg-white p-8 rounded-lg">
+                                        <h2 className="text-green-500 font-bold">{plantaSeleccionada.alias}</h2>
+                                        {/*<ul className="flex items-center justify-center gap-5">
+                                            {plantaSeleccionada.map((detalle, index) => (
                                                 <li key={index} className="mb-2">
-                                                    <div className={`${stylesJardin.imagenADetallePlantas}`}
-                                                         key={detalle.nombre}>
+                                                    <div className="imagenADetallePlantas" key={detalle.nombre}>
                                                         <Link href={`/jardin/${detalle.nombre}`}>
-                                                            <Image src={`${detalle.foto}`} alt={`${detalle.nombre}`}
-                                                                   width="300" height="150"/>
+                                                            <Image src={detalle.foto} alt={detalle.nombre} width="300"
+                                                                   height="150"/>
                                                         </Link>
                                                     </div>
                                                 </li>
                                             ))}
-                                        </ul>
+                                        </ul>*/}
+                                        <button className="font-bold mt-3 py-2 px-4 rounded text-white bg-green-500"
+                                                onClick={() => setPopupVisible(false)}>
+                                            Cerrar
+                                        </button>
                                     </div>
-                                    <button
-                                        className={`${stylesJardin.boton} font-bold mt-3 py-2 px-4 rounded text-white`}
-                                        onClick={() => setPopupVisible(false)}>Cerrar
+                                </div>
+                            </>
+                        )}
+                        {/*{popupVisible && plantaSeleccionada && (
+                            <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
+                                <div className="bg-white p-8 rounded-lg">
+                                    <h2 className="text-green-500 font-bold">{cat}</h2>
+                                    <ul className="flex items-center justify-center gap-5">
+                                        {plantaSeleccionada.map((detalle, index) => (
+                                            <li key={index} className="mb-2">
+                                                <div className="imagenADetallePlantas" key={detalle.nombre}>
+                                                    <Link href={`/jardin/${detalle.nombre}`}>
+                                                        <Image src={detalle.foto} alt={detalle.nombre} width="300" height="150" />
+                                                    </Link>
+                                                </div>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                    <button className="font-bold mt-3 py-2 px-4 rounded text-white bg-green-500" onClick={() => setPopupVisible(false)}>
+                                        Cerrar
                                     </button>
                                 </div>
                             </div>
-                        )}
+                        )}*/}
                     </section>
                 </div>
             </main>
