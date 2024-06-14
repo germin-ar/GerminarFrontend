adde4c5e105906e03271b9fe5db1478d840c3a48
Agostina <aherdeiro@alumno.unlam.edu.ar>
2024-06-14 11:21:18 -0300

    home mejoras - wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,43 +18,47 @@ export default function Home() {
                     <HerramientasDeCultivo targetRef={targetRef}/>
                 </div>
             </section>
-            <section className={`${styles.nuestraPropuesta} flex justify-center items-center flex-col md:flex-row`}>
-                <div className="md:w-1/2">
-                    <div
-                        className={`${styles.contenidoPropuesta} flex flex-col items-center md:items-start text-center md:text-left`}>
-                        <h2 className={`${styles.tituloSecundario} mb-4`}>Nuestra propuesta</h2>
-                        <p>¿Sueñas con alimentos frescos directamente de tu casa? Nuestra aplicación transforma cada
+            <section>
+                <div className="flex flex-col md:flex-row items-center p-6 mb-4">
+                    <div className="md:w-1/2 px-20">
+                        <h2 className={`${styles.tituloSecundario} ${BalooBhaina2.className} mb-4`}>Nuestra propuesta</h2>
+                        <p className="leading-relaxed">
+                            ¿Sueñas con alimentos frescos directamente de tu casa? Nuestra aplicación transforma cada
                             rincón de tu hogar en un jardín próspero. Olvídate de las limitaciones de espacio y la falta
                             de experiencia. Te guiaremos paso a paso para cultivar con éxito en balcones, terrazas y
-                            hasta interiores.</p>
+                            hasta interiores.
+                        </p>
                     </div>
-                </div>
-                <div className="md:w-1/2">
-                    <div className={`${styles.imagenPropuesta} flex justify-center md:justify-end`}>
-                        <Image src="/ecosistema.jpg" alt="Ecosistema" width="500" height="300"/>
+                    <div className="md:w-1/2 flex justify-center md:pl-8 mb-4 md:mb-0">
+                        <div className="max-w-full">
+                            <Image
+                                className={`rounded-lg shadow-md`}
+                                src="/ecosistema.jpg"
+                                alt="Ecosistema"
+                                width="500"
+                                height="300"
+                            />
+                        </div>
                     </div>
                 </div>
             </section>
 
-
             <section
-                className={`${styles.contenedorPropuesta} grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-6 justify-items-center`}>
+                className={`bg-[#EFE8D6] grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-6 justify-center shadow-lg`}>
                 <div className={`${styles.tamanoRecomendacionCards} flex justify-center  gap-3 py-10`}>
                     <div className="flex-1 flex items-center w-full justify-center">
                         <Image className={`${styles.imagenes}`} src="/calendario.jpg" alt="Ecosistema" width="200"
                                height="200"/>
                     </div>
-                    <div className="flex-1">
-                        <div className={`${styles.cardsPropuesta}`}>
-                            <h3 className={`${BalooBhaina2.className}`}>Administra
-                                tus propias notificaciones</h3>
+                    <div className="flex-1 flex items-center">
+                        <div className={"flex flex-col"}>
+                            <h3 className={`${BalooBhaina2.className} text-nowrap`}>Administra tus notificaciones</h3>
                             <p>
-                                Personalizar alertas para el cuidado de tus plantas. Configura recordatorios para riego,
-                                fertilización, poda y otras tareas según tu horario. Personaliza el contenido y el
-                                método de
-                                notificación para una gestión eficiente de tus plantas</p>
+                                Personaliza alertas para cuidar tus plantas. Configura recordatorios para riego, fertilización, poda y más según tu horario y preferencias.
+                            </p>
                             <button
-                                className={`${styles.botonCards} w-1/2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded`}>Administrar
+                                className={`${styles.botonCards} mt-2 w-fit text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:bg-[#76A832] active:bg-[#639122] active:scale-75`}>
+                                Administrar
                             </button>
                         </div>
                     </div>
@@ -64,18 +68,15 @@ export default function Home() {
                         <Image className={`${styles.imagenes}`} src="/seguimiento.jfif" alt="Ecosistema" width="200"
                                height="200"/>
                     </div>
-                    <div className="flex-1">
-                        <div className={`${styles.cardsPropuesta}`}>
+                    <div className="flex-1 flex items-center">
+                        <div className={"flex flex-col"}>
                             <h3 className={`${BalooBhaina2.className}`}>Haz un
                                 seguimiento de tu
                                 progreso</h3>
-                            <p>Lleva un registro detallado de tu experiencia de cultivo con nuestra función de
-                                seguimiento.
-                                Anota cuándo plantas, riegas y cosechas, y recibe recordatorios automáticos para las
-                                tareas
-                                de cuidado de tus plantas. </p>
+                            <p>Registra tu experiencia de cultivo con nuestra función de seguimiento. Anota plantaciones, riegos y cosechas con recordatorios automáticos.</p>
                             <button
-                                className={`${styles.botonCards} w-1/2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded`}>Seguir
+                                className={`${styles.botonCards} mt-2 w-fit text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:bg-[#76A832] active:bg-[#639122] active:scale-75`}>
+                                Seguir
                             </button>
                         </div>
                     </div>
@@ -85,17 +86,15 @@ export default function Home() {
                         <Image className={`${styles.imagenes}`} src="/tutoriales.png" alt="Ecosistema" width="200"
                                height="200"/>
                     </div>
-                    <div className="flex-1">
-                        <div className={`${styles.cardsPropuesta}`}>
-                            <h3 className={`${BalooBhaina2.className}`}>Busca y
+                    <div className="flex-1 flex items-center">
+                        <div className={"flex flex-col"}>
+                            <h3 className={`${BalooBhaina2.className} text-nowrap`}>Busca y
                                 aprende
                                 tutoriales</h3>
-                            <p>Domina el arte de la horticultura urbana con nuestros tutoriales paso a paso. Aprende
-                                cómo
-                                cuidar de tus plantas, identificar y solucionar problemas, y maximizar tus cosechas. Los
-                                tutoriales son fáciles de seguir y adecuados para todos.</p>
+                            <p>Aprende horticultura urbana con nuestros tutoriales paso a paso. Cuida tus plantas, resuelve problemas y maximiza tus cosechas de manera fácil y accesible.</p>
                             <button
-                                className={`${styles.botonCards} w-1/2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded`}>Biblioteca
+                                className={`${styles.botonCards} mt-2 w-fit text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:bg-[#76A832] active:bg-[#639122] active:scale-75`}>
+                                Biblioteca
                             </button>
                         </div>
                     </div>
@@ -105,16 +104,13 @@ export default function Home() {
                         <Image className={`${styles.imagenes}`} src="/ideal.png" alt="Ecosistema" width="200"
                                height="200"/>
                     </div>
-                    <div className="flex-1">
-                        <div className={`${styles.cardsPropuesta}`}>
-                            <h3 className={`${BalooBhaina2.className}`}>Descubre Tu
-                                planta Ideal para
-                                tu jardín</h3>
-                            <p>Encuentra las plantas perfectas para tu espacio con nuestra herramienta de recomendación
-                                personalizada. Simplemente ingresa tus preferencias y te ayudaremos a seleccionar las
-                                plantas que mejor se adapten a tu situación.</p>
+                    <div className="flex-1 flex items-center">
+                        <div className={"flex flex-col"}>
+                            <h3 className={`${BalooBhaina2.className}`}>Descubre tu planta ideal para tu jardín</h3>
+                            <p>Encuentra plantas perfectas para tu espacio con nuestra herramienta de recomendación personalizada.</p>
                             <button
-                                className={`${styles.botonCards} w-1/2  text-white font-bold py-2 px-4 rounded`}>Descubrir
+                                className={`${styles.botonCards} mt-2 w-fit text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:bg-[#76A832] active:bg-[#639122] active:scale-75`}>
+                                Descubrir
                             </button>
                         </div>
                     </div>
