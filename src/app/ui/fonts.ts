00a8fee6e97b5c0165870df39c7bba070650f0eb
Agostina <aherdeiro@alumno.unlam.edu.ar>
2024-06-27 17:59:39 -0300

    arreglos en caracts planta

--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -2,4 +2,4 @@ import { Baloo_Bhaina_2, Poppins } from 'next/font/google'
 
 
 export const BalooBhaina2 = Baloo_Bhaina_2({subsets: ['latin'], weight: '600'})
-export const PoppinsTexto = Poppins({subsets: ['latin'], weight: '200'})
+export const PoppinsTexto = Poppins({subsets: ['latin'], weight: '300'})
