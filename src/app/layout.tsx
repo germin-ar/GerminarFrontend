2104c7bb990cb07acf00438f7268ebd4e4f57f90
Agostina <aherdeiro@alumno.unlam.edu.ar>
2024-06-27 22:21:04 -0300

    title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,44 @@
-import {Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import {PoppinsTexto} from "@/app/ui/fonts";
+import { PoppinsTexto } from "@/app/ui/fonts";
 import styles from "./home.module.css";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
+import { Metadata } from 'next';
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: {
+        template: "Germin.ar - %s",
+        default: "Germin.ar - Home",
+    },
+};
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
+    children,
+}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <html lang="en">
 
-        <head>
-            <link rel="icon" href="/favicon-germinar.ico" sizes="16x16 32x32 48x48 64x64 256x256" type="image/x-icon" />
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <title>Home</title>
-        </head>
+            <head>
+                <link rel="icon" href="/favicon-germinar.ico" sizes="16x16 32x32 48x48 64x64 256x256" type="image/x-icon" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </head>
 
-        <body className={`${inter.className} ${PoppinsTexto.className} ${styles.body} antialiased relative`}>
+            <body className={`${inter.className} ${PoppinsTexto.className} ${styles.body} antialiased relative`}>
 
-            <Header/>
+                <Header />
 
-            <main className={`${styles.content}`}>
-                {children}
-            </main>
+                <main className={`${styles.content}`}>
+                    {children}
+                </main>
 
-            <Footer/>
+                <Footer />
 
-        </body>
+            </body>
 
         </html>
     );
